5d25979cdc6ef909afac26d19f77c6e55b046701
Djamel Ourhribel <la238559@student.helha.be>
2025-08-01 21:19:20 +0200

    class pour les types de spectacle

---
+++ Model/Spectacle/SpectacleType.php
@@ -0,0 +1,17 @@
+<?php
+
+class SpectacleType
+{
+    private const Invalid = 0;
+    public const Theatre = 1;
+    public const ConcertRock = 2;
+    public const ConcertClassique = 3;
+    public const Humoriste = 4;
+    public const Danse = 5;
+    private const Max = 6;
+
+    public static function isValid($type)
+    {
+        return $type > self::Invalid && $type < self::Max;
+    }
+}
