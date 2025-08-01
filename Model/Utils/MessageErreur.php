051f1b5cb90ac02ed571d1643ed6333660ff15f0
Djamel Ourhribel <la238559@student.helha.be>
2025-08-01 21:17:24 +0200

    class pour les messages d'erreurs

--- Model/Utils/MessageErreur.php
+++ Model/Utils/MessageErreur.php
@@ -2,5 +2,12 @@
 
 class MessageErreur
 {
-    
+    public $title;
+    public $message;
+
+    public function __construct($newTitle = null, $newMessage = null)
+    {
+        $this->title = $newTitle ?? "Erreur";
+        $this->message = $newMessage ?? "Une erreur est survenue. Veuillez réessayer plus tard.";
+    }
 }
