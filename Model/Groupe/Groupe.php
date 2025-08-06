1e8a94fd777ea3fc24f6773e014a9e7eafc74f54
LA238559-Ourhribel <la238559@student.helha.be>
2025-08-06 21:15:52 +0200

    developpement de la methode getGroupeData

---
+++ Model/Groupe/Groupe.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Groupe
+{
+    private $groupeId;
+    private $pdo;
+
+    public function __construct($groupeId = null)
+    {
+        $this->groupeId = $groupeId;
+        $this->pdo = Database::getInstance()->getConnection();
+    }
+
+    public function creerGroupe($nomGroupe)
+    {
+        $query = "INSERT INTO Groupe_Spectacle (nom_groupe, date_formation_groupe) VALUES (?, NOW())";
+        $stmt = $this->pdo->prepare($query);
+        if ($stmt->execute([$nomGroupe]))
+        {
+            $this->groupeId = $this->pdo->lastInsertId();
+            return $this->groupeId;
+        }
+        return false;
+    }
+       
+    public function getGroupeData()
+    {
+        $query = "SELECT * FROM Groupe_Spectacle WHERE groupe_id = ?";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$this->groupeId]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+}
