b5ac01b46fae97d247c05ce9e9131c81ed38b8c2
LA236333-Arifi <la236333@student.helha.be>
2025-08-08 00:22:32 +0200

    Ajout de la méthode pour rechercher des utilisateurs dans la liste des utilisateurs, qu'ils soient inscrits ou en attente

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -94,4 +94,52 @@ class UserController
         $viewRenderer = new ViewRenderer("View/UserAccessList.php", $viewData);
         $viewRenderer->render();
     }
+
+    public function searchUsers()
+    {
+        if (!UserConnectionUtils::isAdminConnected())
+        {
+            http_response_code(403);
+            ViewRenderer::error(new MessageErreur("Accès refusé", "Réservé aux administrateurs"));
+            return false;
+        }
+
+        if (RequestUtils::isPostMethod() == false)
+        {
+            // Définir un code HTTP 405 (Method Not Allowed)
+            http_response_code(405);
+
+            // On setup le message d'erreur pour la vue
+            ViewRenderer::error(new MessageErreur("Chargement de la page impossible", "Méthode non supportée"));
+            return false;
+        }
+
+        header('Content-Type: application/json');
+
+        $query = trim($_GET['query'] ?? '');
+        $page = max(1, (int)($_GET['page'] ?? 1));
+        $limit = max(UserList::LimitDefault, (int)($_GET['limit'] ?? UserList::LimitDefault));
+
+        if (empty($query))
+        {
+            echo json_encode(
+                [
+                    'status' => 'error',
+                    'message' => 'Champ de recherche vide.'
+                ]);
+                
+            return false;
+        }
+
+        $userList = new UserList($page, $limit);
+        $userList->searchUsersByQuery($query);
+
+        echo json_encode(
+        [
+            'results' => $userList->getUsers(),
+            'totalUsers' => $userList->getTotalUsers(),
+            'totalPages' => $userList->getTotalPages(),
+            'currentPage' => $page
+        ]);
+    }
 } 
