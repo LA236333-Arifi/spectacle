019237024a5dbde18a8d9f545d417b45d87ef11e
LA236333-Arifi <la236333@student.helha.be>
2025-08-08 23:17:55 +0200

    Ajout des méthodes pour ajouter une séance ainsi que de la déplacer.

--- Model/Seance/Seance.php
+++ Model/Seance/Seance.php
@@ -29,4 +29,31 @@ class Seance
         }
         return null;
     }
+
+    public function ajouterSeance(SeanceData $data)
+    {
+        $query = "INSERT INTO Seance (date_soiree_seance, date_ajout_seance, utilisateur_id, statut_seance_id, spectacle_id) VALUES (?, NOW(), ?, ?, ?)";
+        $stmt = $this->pdo->prepare($query);
+        if ($stmt->execute([
+            $data->getDateSoiree(),
+            $data->getUtilisateurId(),
+            $data->getStatutSeanceId(),
+            $data->getSpectacleId()
+        ]))
+        {
+            $this->seanceId = $this->pdo->lastInsertId();
+            return $this->seanceId;
+        }
+        return false;
+    }
+    
+    public function deplacerSeance($nouvelleDate)
+    {
+        $query = "UPDATE Seance SET date_soiree_seance = ? WHERE seance_id = ?";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$nouvelleDate, $this->seanceId]);
+
+        // Vérifie si une ligne a été modifiée
+        return $stmt->rowCount() > 0;
+    }
 }
