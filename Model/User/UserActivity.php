323dd0b2e2d90b4a8a5efe21f8261c7dad5d774f
LA236333-Arifi <la236333@student.helha.be>
2025-08-08 23:44:45 +0200

    Fix d'un bug ou on utilisait l'ancien système pour changer l'activité d'un utilisateur via les 2 booléens. Maintenant c'est un champ statut_id

--- Model/User/UserActivity.php
+++ Model/User/UserActivity.php
@@ -3,16 +3,21 @@
 class UserActivity
 {
     private $userId = null;
-    private $userActif = null;
+    private $userStatut = null;
 
     public function __construct($newId)
     {
         $this->userId = $newId;
     }
 
-    public function getUserActif()
+    public function getUserStatut()
     {
-        return $this->userActif;
+        return $this->userStatut;
+    }
+
+    public function isUserStatutActif()
+    {
+        return $this->userStatut == UserStatut::Valide_Et_Actif;
     }
 
     public function storeUserActivity()
@@ -25,28 +30,30 @@ class UserActivity
         $db = Database::getInstance()->getConnection();
 
         // Récupérer le statut actuel
-        $stmt = $db->prepare("SELECT actif_utilisateur FROM utilisateur WHERE utilisateur_id = ?");
+        $stmt = $db->prepare("SELECT statut_utilisateur_id FROM utilisateur WHERE utilisateur_id = ?");
         $stmt->execute([$this->userId]);
-        $this->userActif = $stmt->fetchColumn();
+        $this->userStatut = $stmt->fetchColumn();
 
-        return true;
+        return $this->userStatut != UserStatut::NonValide_Et_Inactif;
     }
 
     public function toggleUserActivity()
     {
         $db = Database::getInstance()->getConnection();
 
-        if (!isset($userActif))
+        if (!isset($this->userStatut))
         {
             return false;
         }
 
         // Inverser le statut
-        $newStatus = $this->userActif ? 0 : 1;
-        $stmt = $db->prepare("UPDATE utilisateur SET actif_utilisateur = ? WHERE utilisateur_id = ?");
+        $newStatus = ($this->userStatut == UserStatut::Valide_Et_Inactif) ? 
+                                           UserStatut::Valide_Et_Actif : 
+                                           UserStatut::Valide_Et_Inactif;
+        $stmt = $db->prepare("UPDATE utilisateur SET statut_utilisateur_id = ? WHERE utilisateur_id = ?");
         if ($stmt->execute([$newStatus, $this->userId]))
         {
-            $this->userActif = $newStatus;
+            $this->userStatut = $newStatus;
             return true;
         }
 
