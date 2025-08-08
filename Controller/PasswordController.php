f26aaa4367c448f5b9110c28078f931ca1978f9a
Djamel Ourhribel <la238559@student.helha.be>
2025-08-08 23:27:37 +0200

    developpement de la methode resetPassword dans la class controller PasswordController

---
+++ Controller/PasswordController.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * Contrôleur pour la gestion des mots de passe
+ * Gère la réinitialisation et le changement de mots de passe
+ */
+class PasswordController 
+{
+    private $security;
+    
+    public function __construct() 
+    {
+        $this->security = new Security(true);
+    }
+
+    public function resetPassword()
+    {
+        if (UserConnectionUtils::isUserConnected())
+        {
+            // Code 400 (Bad request)
+            http_response_code(400);
+            ViewRenderer::error(new MessageErreur("Chargement de la page impossible", "Veuillez vous déconnecter pour réinitialiser votre mot de passe."));
+            return false;
+        }
+
+        if (RequestUtils::isGetMethod())
+        {
+            // Génére le token CSRF
+            $csrf_token = $this->security->genererCSRFToken();
+            
+            $viewRenderer = new ViewRenderer("View/ResetPassword.php", ['token_csrf' => $csrf_token]);
+            $viewRenderer->render();
+            
+            return true;
+        }
+        else if (RequestUtils::isPostMethod()) 
+        {
+            // Définir un code HTTP 400 (Bad Request) par défaut
+            http_response_code(400);
+
+            // On renvoie du JSON par défaut (AJAX)
+            header("Content-Type: application/json");
+
+            // Vérification du token CSRF
+            if (!$this->security->checkCSRFToken($_POST['csrf_token'] ?? ''))
+            {
+                http_response_code(403);
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => "Token CSRF invalide."
+                ]);
+                return false;
+            }
+
+            // On vérifie que l'email a bien été fournie
+            $email = $_POST['mail_utilisateur'] ?? null;
+            if (empty($email)) 
+            {
+                // Réponse JSON avec le message d'erreur
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'Adresse email invalide.'
+                ]);
+                return false;
+            }
+
+            // Vérifier le format de l'email
+            if (!UserDataValidator::verifyEmailFormat($email)) 
+            {
+                // Réponse JSON avec le message d'erreur
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'Le format de l\'adresse email est invalide.'
+                ]);
+                return false;
+            }
+
+            // Vérifier si l'email existe dans la base de données
+            $userId = (new UserLogin($email))->getActiveUserIdWithEmail();
+
+            // Si l'utilisateur n'existe pas, on ne renvoie pas d'info
+            if ($userId === false) 
+            {
+                // Réponse JSON avec le message d'erreur
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'Aucun compte valide associé à cet email.'
+                ]);
+                return false;
+            }
+
+            // Sauvegarder le token en base de données
+            $token = new TokenResetPassword();
+            if (!$token->setUserToken($userId)) 
+            {
+                // Réponse JSON avec le message d'erreur
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'Erreur: Utilisateur invalide'
+                ]);
+
+                return false;
+            }
+
+            // On forme le lien suivant : localhost/{nom_du_site}/password/change?token={token_value}
+            $reset_link = $_ENV['SITE_URL'] ?? "localhost";  
+            $reset_link .= BASE_URL . "/password/change?token=" . $token->getToken();
+
+            $emailSender = new EmailSender($email);
+            $emailSender->setSubject(PasswordResetEmail::getSubject([]));
+            $emailSender->setBody(PasswordResetEmail::getEmailContent(['resetLink' => $reset_link]));
+
+            // On vérifie que l'email a bien été envoyé
+            $mailEnvoye = $emailSender->sendMail();
+            if ($mailEnvoye)
+            {
+                // Définir un code HTTP 200 (Succès)
+                http_response_code(200);
+
+                // Réponse JSON avec le message de succès
+                $retourJson = 
+                [
+                    'status' => 'success',
+                    'message' => 'Un email de réinitialisation a été envoyé.'
+                ];
+            }
+            else 
+            {
+                // Réponse JSON avec le message d'erreur
+                $retourJson = 
+                [
+                    'status' => 'error',
+                    'message' => "'L'envoi de l'email a échoué."
+                ];
+            }
+
+            // Si jamais on veut debug le token pour le reset de mdp
+            $debugMailToken = filter_var($_ENV['DEBUG_MAIL_TOKEN'] ?? false, FILTER_VALIDATE_BOOLEAN);
+            if ($debugMailToken)
+            {
+                $retourJson['debug'] = 'Debug: voici le lien pour reset le mot de passe : ' . $reset_link;
+            }
+
+            echo json_encode($retourJson);
+            return $mailEnvoye;
+        } 
+        else
+        {
+            // Définir un code HTTP 405 (Method Not Allowed)
+            http_response_code(405);
+
+            // On setup le message d'erreur pour la vue
+            ViewRenderer::error(new MessageErreur("Chargement de la page impossible", "Méthode non supportée"));
+            return false;
+        }
+    }
+
+}
