c872a5d49000babaa20c31321217cf7a1333acbb
Djamel Ourhribel <la238559@student.helha.be>
2025-08-14 18:55:18 +0200

    ajout du autoload et modification du viewrenderer

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -1,7 +1,17 @@
 <?php
 
+require_once 'autoload.php';
+
 class DashboardController
 {
+    private $security;
+    
+    public function __construct() 
+    {
+        // On crée notre session sécurisée via le constructeur
+        $this->security = new Security(true);
+    }
+
     public function index()
     {
         // Ici on définit la page d'accueil comme étant celle de la programmation
@@ -10,16 +20,13 @@ class DashboardController
 
     public function programmation()
     {
-        $viewRenderer = new ViewRenderer("Visitor/Programmation", 
-        [
-            'seances' => (new SeanceController())->listSeances()
-        ]);
+        $viewRenderer = new ViewRenderer("View/Visitor/Programmation.php", []);
         $viewRenderer->render();
     }
     
     public function calendrier()
     {
-        $viewRenderer = new ViewRenderer("Visitor/Calendrier", []);
+        $viewRenderer = new ViewRenderer("View/Visitor/Calendrier.php", []);
         $viewRenderer->render();
     }
 }
