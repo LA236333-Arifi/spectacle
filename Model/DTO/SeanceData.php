3d3878cb408830c1d05b3861ed46d141bfe856ef
LA238559-Ourhribel <la238559@student.helha.be>
2025-08-18 09:55:39 +0200

    modification de SeanceData.php et SpectacleData.php

--- Model/DTO/SeanceData.php
+++ Model/DTO/SeanceData.php
@@ -4,19 +4,16 @@ class SeanceData
 {
     private string $dateSoiree;
     private string $dateAjout;
-    private int $statutSeanceId;
     private int $utilisateurId;
     private int $spectacleId;
 
     public function __construct(
         string $dateSoiree,
-        $statutSeanceId, 
         $utilisateurId = null, 
         $spectacleId = null
     )
     {
         $this->dateSoiree = $dateSoiree;
-        $this->statutSeanceId = $statutSeanceId;
         $this->utilisateurId = $utilisateurId;
         $this->spectacleId = $spectacleId;
     }
@@ -39,10 +36,5 @@ class SeanceData
     public function getDateAjout()
     {
         return $this->dateAjout;
-    }
-
-    public function getStatutSeanceId()
-    {
-        return $this->statutSeanceId;
     }
 }
